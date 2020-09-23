77e2c75cd812ebb194ddf4d2780749ced1e59304
Ubuntu <ubuntu@ip-10-0-0-8.us-west-2.compute.internal>
2020-09-23 05:45:21 +0000

    need larger ec2

---
+++ src/ETLPipeline/config.py
@@ -0,0 +1,4 @@
+bucket_name_zipped_file="100booktest"
+bucket_name_xml_file="stackoverflowdumpdata"
+file_path = '../../url_list.txt'
+
---
+++ src/ETLPipeline/unzip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+url=$1
+#echo $url
+
+file_name=$(echo $url| rev|cut -d '/' -f 1 | rev )
+file_type=$(echo $file_name | rev | cut -d '-' -f-1 | rev)
+
+echo Filename$file_name
+echo Filetype$file_type
+
+7za e "./tmp/"$file_name 
+
+echo "Finished unzip"
+echo "Delete the files"
