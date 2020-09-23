1a146eb192193c1948713dc6ccfc8bd8f7a6c4e6
Ubuntu <ubuntu@ip-10-0-0-12.us-west-2.compute.internal>
2020-09-23 15:30:37 +0000

    module for download upload

--- src/ETLPipeline/unzip.sh
+++ src/ETLPipeline/unzip.sh
@@ -6,10 +6,10 @@ url=$1
 file_name=$(echo $url| rev|cut -d '/' -f 1 | rev )
 file_type=$(echo $file_name | rev | cut -d '-' -f-1 | rev)
 
-echo Filename$file_name
-echo Filetype$file_type
+#echo Filename$file_name
+#echo Filetype$file_type
 
 7za e "./tmp/"$file_name 
 
-echo "Finished unzip"
-echo "Delete the files"
+#echo "Finished unzip"
+#echo "Delete the files"
