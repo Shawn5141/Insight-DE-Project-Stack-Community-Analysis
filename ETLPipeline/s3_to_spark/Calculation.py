2fe49ffa1667c1d9cf04949a5d1b824fa74d7f75
Ubuntu <ubuntu@ip-172-31-49-77.us-west-2.compute.internal>
2020-10-05 08:20:32 +0000

    mv folder and add filter number to speed up to 20s

---
+++ ETLPipeline/s3_to_spark/Calculation.py
@@ -0,0 +1,123 @@
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import Window
+
+def CalculateTagCount(Questions):
+    # MutualTagCount
+    MutualTagCount = Questions.select("Tags","mappingResult").groupBy("mappingResult","Tags").count().withColumnRenamed("count","Tag Number").sort(desc("count"))
+    # Single Tag Count
+    df = MutualTagCount.select(explode(MutualTagCount.Tags).alias('Tags'),col("Tag Number"))
+    SingleTagCount=df.groupBy(df.Tags).agg({"Tag Number": "sum"}).alias('Tag Number Sum').sort(desc("sum(Tag Number)"))
+#     # Year Tag Count
+#     YearTagCount = Questions.select("Tags","mappingResult","Year","Month").groupBy("mappingResult","Tags","Year","Month").count()\
+#     .withColumnRenamed("count","Tag in each Year/Month")
+    
+    MutualTagCount.show()
+    SingleTagCount.show()
+#     YearTagCount.show()
+    return MutualTagCount,SingleTagCount
+
+def cumSum(df,windowval):
+    #windowval = (Window.partitionBy('class').orderBy('time').rangeBetween(Window.unboundedPreceding, 0))
+    df_w_cumsum = df.withColumn('cum_sum', F.sum('value').over(windowval))
+    df_w_cumsum.show()
+
+
+def CalculateYearTagCount(Questions):
+    # Calculate for once
+    
+    cols = ["Tags", "mappingResult","Year"]
+    w = Window.partitionBy(cols)
+    YearCount = Questions.select("Tags","Year","Month","mappingResult").withColumn("count", count("mappingResult").over(w)).orderBy("count").dropDuplicates(["Tags","Year"]).drop('Month')
+    YearCount.sort(desc('count')).show()
+    windowval = (Window.partitionBy('Tags').orderBy('Year').rangeBetween(Window.unboundedPreceding, 0))
+    PrefixSumYearCount = YearCount.withColumn('cum_sum', sum('count').over(windowval)).drop('count')
+    PrefixSumYearCount.show()
+    return YearCount,PrefixSumYearCount
+    
+def CalculateRangeYearTagCount(PrefixSumYearCount,BeginYear,EndYear):
+    # Calculate based on precompute table
+    
+    cols = ["Tags"]
+    w = Window.partitionBy(cols)
+    df = PrefixSumYearCount.withColumn("GroupCount", count("Tags").over(w))
+    w = Window.partitionBy("Tags")
+    # Filter out based on begin year and end year
+    df=df.filter((df.Year>=BeginYear-1) & (df.Year<=EndYear))
+    rangeYearTagCount = df.withColumn('YearTagCount', when(col('GroupCount')>=2,max('cum_sum').over(w)- when(min('Year').over(w) == BeginYear-1,min('cum_sum').over(w)).otherwise(0)).otherwise(max('cum_sum').over(w))).dropDuplicates(["Tags"]).drop("Year").sort(desc('YearTagCount'))
+    rangeYearTagCount.show(30, truncate = False)
+    return rangeYearTagCount
+    
+def CalculateRangeYearTagCount2(preTable,postTable,filterNum):
+    # Subtract post and pre and get 
+    preTable = preTable.withColumnRenamed("cum_sum","pre_cum_sum").withColumnRenamed("Year","pre_Year").withColumnRenamed("mappingResult","pre_mappingResult").withColumnRenamed("Tags","pre_Tags")
+    
+    joinTable = postTable.join(preTable,postTable.Tags == preTable.pre_Tags,how='left')
+    joinTable = joinTable.withColumn("YearTagCount",joinTable.cum_sum-when(joinTable.pre_cum_sum.isNull(),0).otherwise(joinTable.pre_cum_sum))
+    return joinTable.select("Tags","YearTagCount").filter(joinTable.YearTagCount>filterNum)
+
+def CalculateSingleTagCount(rangeYearTagCount):
+    rangeYearTagCount = rangeYearTagCount.withColumn("Original_Tags",rangeYearTagCount.Tags)
+    df = rangeYearTagCount.select("Original_Tags",explode(rangeYearTagCount.Tags).alias('Tags'),col("YearTagCount"))
+    edge = df.select("Original_Tags","Tags","YearTagCount")
+    
+    singleTagCount=df.groupBy(df.Tags).agg({"YearTagCount": "sum"}).withColumnRenamed("sum(YearTagCount)","singleTagCount")
+    singleTagCount.show(10, truncate = False)
+    edge.show(10,truncate=False)
+    return singleTagCount,edge
+
+    
+def CalculateAnswerTime(Questions,Answers):
+#     Answers.show()
+#     Questions.show()
+    Answers = Answers.select(col('ParentId'),col('Id').alias('AnswersID'),\
+                                 col('CreationDate').alias('AnswersCreationDate'))
+                                           
+    Answers = Answers.withColumn("MinAnswersCreationDate", col("AnswersCreationDate").cast("timestamp"))\
+    .groupBy(col("ParentId"))\
+    .agg(min(col("AnswersCreationDate")).alias("Min_of_AnswersCreationDate"))
+    
+    Questions = Questions.join(Answers, Questions.Id == Answers.ParentId,how='inner')
+    
+    timeFmt = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+    timeDiff = (unix_timestamp('CreationDate', format=timeFmt)\
+            - unix_timestamp('Min_of_AnswersCreationDate', format=timeFmt))
+    Questions = Questions.withColumn('AnswerTime',timeDiff ) 
+    Questions.show()
+    return Questions
+
+def CalculateActiveUser(Questoins,Answers,Users):
+   
+    Users = Users.select(col("Id").alias('UserId'),"Reputation","CreationDate","DisplayName")
+    Answers=Answers.filter(col("OwnerUserId").isNotNull()).select("Id","ParentId","OwnerUserId",col("AnsweredYear"),col("AnsweredMonth"))
+    
+    #answerCount = Answers.count()
+    
+    Users_Answer_join = Answers.join(Users, Users.UserId == Answers.OwnerUserId,how='left')
+    Users_Answer_join = Users_Answer_join.withColumnRenamed('Id','AnswerId')
+    #Users_Answer_joinCount = Users_Answer_join.count()
+    
+    
+    Questoins = Questoins.filter((col('AcceptedAnswerId').isNotNull() \
+                                & col("Id").isNotNull())).select("Id","Tags","mappingResult","AcceptedAnswerId")
+    
+    Users_Answer_Questoins_join = Users_Answer_join.join(Questoins,Users_Answer_join.AnswerId==Questoins.AcceptedAnswerId,how='left') 
+    
+            
+    Users_Answer_Questoins_join = Users_Answer_Questoins_join.groupBy("AnsweredYear","AnsweredMonth","Tags","UserId").count()\
+            .withColumnRenamed("count","User_count_per_tag").filter(col('Tags').isNotNull()).sort(desc("User_count_per_tag"))
+    
+    #Users_Answer_Questoins_joinCount2 = Users_Answer_Questoins_join.count()
+    
+    Users_Answer_Questoins_join = Users_Answer_Questoins_join\
+                               .withColumnRenamed("AnsweredYear","Year")\
+                               .withColumnRenamed("AnsweredMonth","Month")
+    
+#     df=spark.createDataFrame([(answerCount,Users_Answer_joinCount,Users_Answer_Questoins_joinCount2)],[('answerCount','Users_Answer_joinCount','Users_Answer_Questoins_joinCount2')])
+    #df.show()
+    
+    return Users_Answer_Questoins_join
+    #return None
+    
---
+++ ETLPipeline/s3_to_spark/TagPreprocessing.py
@@ -0,0 +1,100 @@
+from pyspark.sql.functions import year, month, dayofmonth
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import Window
+from configFile import config
+import random
+import sys
+
+
+def Preprocess(spark,df_Posts):
+    Posts,Answers =SeperateAndDrop(df_Posts)
+    TagSynMapping=TagDownload(spark)
+    b_TagSynMapping = Broadcast(spark,TagSynMapping)
+    Posts = MappingTag(b_TagSynMapping,Posts)
+    return Posts,Answers
+
+
+def SeperateAndDrop(df_Posts):
+    
+    columns_to_drop = ['LastEditorUserId','LastEditorDisplayName','Body','LastEditDate','LastActivityDate','Title']
+    df_Posts = df_Posts.drop(*columns_to_drop)
+    
+    Question,Answers = df_Posts.filter(df_Posts.PostTypeId == 1).filter((col('AcceptedAnswerId').isNotNull())),\
+                      df_Posts.filter(df_Posts.PostTypeId == 2)
+    columns_to_drop_Question,columns_to_drop_Answers =['PostTypeId','ParentId','CommentCount','AnswerCount','FavoriteCount'], ['PostTypeId','AcceptedAnswerId','AnswerCount','FavoriteCount','Tags','Title','ViewCount']
+    Question= Question.withColumn("Year",year(Question.CreationDate)).withColumn("Month",month(Question.CreationDate))
+    
+    Answers = Answers.withColumn("AnsweredYear",year(Question.CreationDate)).withColumn("AnsweredMonth",month(Question.CreationDate))
+    
+    return Question.drop(*columns_to_drop_Question),Answers.drop(*columns_to_drop_Answers)
+    
+
+def TagDownload(spark):
+    conf = config()
+    TagName=spark.read.option("header",True).csv(conf.TagNameLink)
+    TagSyn=spark.read.option("header",True).csv(conf.TagSynLink)
+    TagSyn.drop(*['CreationDate','TargetTagName','OwnerUserId','AutoRenameCount'\
+                  ,'LastAutoRename','Score','ApprovedByUserId','ApprovalDate'])
+    
+    TagSyn = TagSyn.withColumn("MAPPING",create_map(
+       'SourceTagName',
+        'Id',
+        'TargetTagName',
+        'Id'))
+    TagSynMapping = TagSyn.select(explode(TagSyn.MAPPING))
+    """
+    synonyms table
+    +---------------+-----+                                                                                                                     |            key|value|                                                                                                                     +---------------+-----+                                                                                                                     |  windows-forms|    3|                                                                                                                     |       winforms|    3|
+    +---------------+-----+
+    """
+    TagSynMapping=SupplementTags2(spark,TagSynMapping,TagName) 
+    return {entry[0]:entry[1] for entry in TagSynMapping.collect()}
+    
+    
+def Broadcast(spark,mapping):
+    return spark.sparkContext.broadcast(mapping)
+    
+def working_fun(mapping_broadcasted):
+    def f(x):
+        tmp = []
+        for word in x:
+            if word and mapping_broadcasted.value.get(word):
+                tmp+=[mapping_broadcasted.value.get(word)]
+        if tmp==[]:
+            return ''
+        tmp.sort()
+        return ','.join(tmp)
+       
+    return udf(f)
+
+
+def MappingTag(TagSynMapping,Posts):
+    return Posts.withColumn('mappingResult', working_fun(TagSynMapping)(col('Tags')))
+      
+
+    
+def SupplementTags2(spark,TagSynMapping,TagName):
+    # http://ec2-44-235-91-5.us-west-2.compute.amazonaws.com:8888/notebooks/findspark.py.ipynb
+    TagSynMapping = TagSynMapping.select(col("key").alias("TagName"),col("value").alias("id"))
+    TagName=TagName.drop('Count').withColumn("id", lit(None).cast(StringType())).select('*')
+    # Concate two column
+    result = TagSynMapping.union(TagName)
+    w = Window.partitionBy('TagName')
+    result=result.select('TagName', 'id', count('TagName').over(w).alias('n'))
+    # Get rid of duplicate
+    result=result.dropDuplicates(subset=['TagName'])
+    # Generate mapping from name to 
+    mapping=result.groupBy(result.id).count().select('id', col('count').alias('n')).withColumn("id2", monotonically_increasing_id()).select('*')
+    #mapping.show()
+    def working_fun2(mapping):
+        def f(x):
+            return mapping.value.get(x)
+
+        return udf(f)
+    mapping = {entry[0]:entry[2] for entry in mapping.collect()}
+    b = spark.sparkContext.broadcast(mapping)
+    result=result.withColumn('new_id',when(col('id').isNotNull()  ,working_fun2(b)(col('id'))).otherwise(monotonically_increasing_id()))
+    result=result.drop('id','n')
+    #result.show()
+    return result
